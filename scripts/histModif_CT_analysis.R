da12dc8c555f6744275d8759587e9909720658d2
jiwang <jingkui.wang@imp.ac.at>
2022-01-20 11:09:43 +0100

    add new script histModif_CT_analysis.R

---
+++ scripts/histModif_CT_analysis.R
@@ -0,0 +1,10 @@
+##########################################################################
+##########################################################################
+# Project: positional memory 
+# Script purpose: process and analyze Akane's histone modification of Cut & Tag
+# Usage example: 
+# Author: Jingkui Wang (jingkui.wang@imp.ac.at)
+# Date of creation: Thu Jan 20 10:43:21 2022
+##########################################################################
+##########################################################################
+
